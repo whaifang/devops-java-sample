1f4b38ff35b792d6af2fcb817bd830bdb74bcac4
whaifang <45645875+whaifang@users.noreply.github.com>
2020-12-08 16:40:38 +0800

    Update HelloWorldControllerTest.java

--- src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
+++ src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
@@ -8,6 +8,6 @@ public class HelloWorldControllerTest {
 
     @Test
     public void testSayHello() {
-        assertEquals("Really appreaciate your stsssssssssars, that's twewewewewhe power of our life.", new HelloWorldController().sayHello());
+        assertEquals("Really appreaciate your stssswwwwwwssssssars, that's twewewewewhe power of our life.", new HelloWorldController().sayHello());
     }
 }
